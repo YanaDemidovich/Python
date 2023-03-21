10972ebd2b561db651a833f6d7c7678796cb71f3
Demidovich <demyanvo1990@gmail.com>
2023-03-21 17:07:50 +0300

    Сохраниа задачу 33

---
+++ seminar_5.py
@@ -0,0 +1,24 @@
+# Задача №33. 
+# Хакер Василий получил доступ к классному журналу и 
+# хочет заменить все свои минимальные оценки на 
+# максимальные. Напишите программу, которая 
+# заменяет оценки Василия, но наоборот: все 
+# максимальные – на минимальные.
+# Input: 5 -> 1 3 3 3 4
+# Output: 1 3 3 3 1
+
+""" marks_list = [int(input("Введите оценку: ")) for _ in range(int(input("Введите кол-во оценок: ")))]
+max = marks_list[0]
+min = marks_list[0]
+for el in marks_list:
+    if el < min:
+        min = el
+    if el > max:
+        max = el
+for ind in range(0, len(marks_list)):
+    if marks_list[ind] == max:
+        marks_list[ind] = min
+print(marks_list) """
+
+# какая-то ерунда вышла
+
